54ac9fbeb845ceeee8888ee1570b593648edafeb
BENEDICTBOISCLAIR <ben.b.boisclair@gmail.com>
2023-03-07 20:47:25 +0700

    Delete firebase.config.js

--- src/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDEfdPkCFkUEXAK5CWE_dIIQhRG2HBCtkE",
-    authDomain: "personal-ai-1d630.firebaseapp.com",
-    projectId: "personal-ai-1d630",
-    storageBucket: "personal-ai-1d630.appspot.com",
-    messagingSenderId: "869119058176",
-    appId: "1:869119058176:web:b9251b755cdada7ecc93e5"
-};
